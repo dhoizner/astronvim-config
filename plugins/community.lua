dccb7e6fdf2b556f91390e3127b4a000ae596047
Dan Hoizner <dan.hoizner@gmail.com>
2023-10-29 14:42:11 -0400

    add language packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,5 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.colorscheme.catppuccin" },
 }
